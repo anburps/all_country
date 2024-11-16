9ddb714f1f04fab32ada03836e9e6972d5d6abba
anburps <anbalaganrps@gmail.com>
2024-11-16 20:11:15 +0530

    project url added

---
+++ src/geolocation/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('countries/', views.get_countries, name='get_countries'),
+    path('states/<int:country_id>/', views.get_states, name='get_states'),
+    path('cities/<str:state_name>/', views.get_cities, name='get_cities'),
+]
