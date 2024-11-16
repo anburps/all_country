f9a88c41d5d660cce3f6dcf6947a5e8b840dc2c2
anburps <anbalaganrps@gmail.com>
2024-11-16 20:10:02 +0530

    get country,state and city functions added

--- src/geolocation/views.py
+++ src/geolocation/views.py
@@ -1,3 +1,23 @@
+# geolocation/views.py
+import requests
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+COUNTRY_API = "http://api.geonames.org/countryInfoJSON?username=your_username"
+STATE_API = "http://api.geonames.org/childrenJSON?geonameId={country_id}&username=your_username"
+CITY_API = "http://api.geonames.org/searchJSON?q={state_name}&username=your_username&featureClass=P&maxRows=10"
+
+def get_countries(request):
+    response = requests.get(COUNTRY_API)
+    countries = response.json().get('geonames', [])
+    return JsonResponse(countries, safe=False)
+
+def get_states(request, country_id):
+    response = requests.get(STATE_API.format(country_id=country_id))
+    states = response.json().get('geonames', [])
+    return JsonResponse(states, safe=False)
+
+def get_cities(request, state_name):
+    response = requests.get(CITY_API.format(state_name=state_name))
+    cities = response.json().get('geonames', [])
+    return JsonResponse(cities, safe=False)
