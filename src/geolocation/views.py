5de90a48d46f6bf6b3d758c91f7b293634691c91
anburps <anbalaganrps@gmail.com>
2024-11-17 20:02:31 +0530

    country get api

--- src/geolocation/views.py
+++ src/geolocation/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 USERNAME = "anbu"  
-COUNTRY_API = f"http://api.geonames.org/countryInfoJSON?username=anbu"
+COUNTRY_API = f"http://api.geonames.org/countryInfoJSON?username=anburpskn"
 STATE_API = "http://api.geonames.org/childrenJSON?geonameId={country_id}&username={username}"
 CITY_API = "http://api.geonames.org/searchJSON?q={state_name}&username={username}"
 
@@ -14,11 +14,14 @@ def fetch_countries(request):
     try:
         response = requests.get(COUNTRY_API)
         response.raise_for_status()  
+        print("+++++++++++++++++++++",response)
         countries = response.json().get('geonames', [])
         return JsonResponse({'countries': countries}, safe=False)
     except requests.exceptions.HTTPError as e:
+        print("error",e)
         return JsonResponse({'error': f"HTTP Error: {str(e)}"}, status=response.status_code)
     except Exception as e:
+        print("________________",e)
         return JsonResponse({'error': f"Error: {str(e)}"}, status=500)
 
 
